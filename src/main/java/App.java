81610b6d86ca0e06235e0a94de8b777d81894349
sravs994 <yash061994@gmail.com>
2026-04-28 16:00:04 +0530

    UpdatedApp.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,7 +7,7 @@ import java.net.InetSocketAddress;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        HttpServer server = HttpServer.create(new InetSocketAddress("0.0.0.0", 8080), 0);
 
         server.createContext("/", new HttpHandler() {
             public void handle(HttpExchange exchange) {
