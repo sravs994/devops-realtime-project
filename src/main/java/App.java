b07300b1bfca6c8785014efcd9eb08854229a045
sravs994 <yash061994@gmail.com>
2026-04-28 15:06:08 +0530

    ModifiedApp.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,8 +1,29 @@
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        while(true) {
-            System.out.println("Hello DevOps Running...");
-            Thread.sleep(10000);
-        }
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+
+        server.createContext("/", new HttpHandler() {
+            public void handle(HttpExchange exchange) {
+                try {
+                    String response = "Hello DevOps Running 🚀";
+                    exchange.sendResponseHeaders(200, response.length());
+                    OutputStream os = exchange.getResponseBody();
+                    os.write(response.getBytes());
+                    os.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        server.start();
+        System.out.println("Server started on port 8080");
     }
 }
